ea3551e433116f8330bd76f81f745f4cf5965907
Jonathan Palma <jonathanpalma9@gmail.com>
2026-07-28 02:58:18 -0600

    refactor: clean file storage service

--- src/WebApi/Controllers/ArchivoController.cs
+++ src/WebApi/Controllers/ArchivoController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Aplicacion.Commands;
 using Aplicacion.Dtos;
 using Aplicacion.Services.Comandos;
+using Dominio.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.IO;
-using Dominio.Models;
 
 namespace WebApi.Controllers
 {
@@ -16,39 +11,30 @@ namespace WebApi.Controllers
     [ApiController]
     public class ArchivoController : ControllerBase
     {
-         public ICommandBus CommandBus { get; }
+        private readonly ICommandBus commandBus;
 
         public ArchivoController(ICommandBus commandBus)
         {
-            CommandBus = commandBus;
+            this.commandBus = commandBus;
         }
 
-        // GET: api/Archivo/5
         [HttpGet("{id}", Name = "Get")]
         public IActionResult Get(int id)
         {
-            var archivo = (DescargarArchivoDto)CommandBus.execute(new ConsultarArchivo { IdArchivo = id });
-            return File(archivo.File,archivo.ContentType,archivo.FileName);
+            var archivo = (DescargarArchivoDto)commandBus.execute(new ConsultarArchivo { IdArchivo = id });
+            return File(archivo.File, archivo.ContentType, archivo.FileName);
         }
 
-        // POST: api/Archivo
         [HttpPost]
         public IResponse Post(IFormFile file)
         {
-            var resultado = CommandBus.execute(new CargarArchivo { File = file});
-            return resultado;
+            return commandBus.execute(new CargarArchivo { File = file });
         }
 
-
-
-        // POST: api/Archivo
-        [HttpPost("registro", Name ="archivoRegistro")]
+        [HttpPost("registro", Name = "archivoRegistro")]
         public IResponse SubirArchivoRegistro(IFormFile file)
         {
-            var resultado = CommandBus.execute(new CargarArchivoRegistro { File = file });
-            return resultado;
+            return commandBus.execute(new CargarArchivoRegistro { File = file });
         }
-
-
     }
 }
